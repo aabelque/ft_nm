5cac118852755c062dbb46791efdba009f29a3e1
aabelque <aabelque@student.42.fr>
2021-03-05 09:30:12 +0100

    add define to print error for preprocessor

--- include/ft_nm.h
+++ include/ft_nm.h
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 13:39:05 by aabelque          #+#    #+#             */
-/*   Updated: 2021/03/05 09:21:46 by aabelque         ###   ########.fr       */
+/*   Updated: 2021/03/05 09:29:20 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,16 @@
 #include <unistd.h>
 #include <limits.h>
 
+#define ERROR(s) prints(s) 
+
 #if __APPLE__
 	#include <mach-o/loader.h>
 	#include <mach-o/nlist.h>
 #elif __linux__
 	#include </usr/include/linux/elf.h>
 	#include </usr/include/bsd/nlist.h>
+#else
+	ERROR("I only work on MAC OS or LINUX system\n")
 #endif
 
 typedef struct	s_env
