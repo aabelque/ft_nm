d36c02d7a3365e53ceee03d4e19335411ff11cec
aabelque <aabelque@student.42.fr>
2021-04-02 15:44:52 +0200

    test elf on linux

--- srcs/srcs_linux/ft_nm_elf.c
+++ srcs/srcs_linux/ft_nm_elf.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 14:33:02 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/02 15:35:46 by aabelque         ###   ########.fr       */
+/*   Updated: 2021/04/02 15:44:48 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ int			nm_elf(char *ptr, char *offset, char *bin)
 {
 	if (check_offset_elf(ptr, offset))
 		return (ft_perror("Corrupted file\n", 0));
-	if ((unsigned char)ptr[EI_MAG0] == 0x7f &&
-			(unsigned char)ptr[EI_MAG1] == 'E' &&
-			(unsigned char)ptr[EI_MAG2] == 'L' &&
-			(unsigned char)ptr[EI_MAG3] == 'F')
+	if ((unsigned char)ptr[ELFMAG0] == 0x7f &&
+			(unsigned char)ptr[ELFMAG1] == 'E' &&
+			(unsigned char)ptr[ELFMAG2] == 'L' &&
+			(unsigned char)ptr[ELFMAG3] == 'F')
 	{
 		prints("It's an elf file\n");
 	}
