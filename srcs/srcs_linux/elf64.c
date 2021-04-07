ab1dee9181793d7a4644a0042689d8162b01706c
aabelque <aabelque@student.42.fr>
2021-04-07 15:17:01 +0200

    test elf on linux

--- srcs/srcs_linux/elf64.c
+++ srcs/srcs_linux/elf64.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 19:22:26 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/07 14:26:29 by aabelque         ###   ########.fr       */
+/*   Updated: 2021/04/07 15:16:56 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,7 @@ int			elf64(char *ptr, char *offset)
 	Elf64_Ehdr			*header;
 
 	header = (Elf64_Ehdr *)ptr;
-	ft_putnbr(header->e_phnum);
-	write(1, "\n", 1);
-	ft_putnbr(header->e_shnum);
-	write(1, "\n", 1);
-	if ((unsigned char)ptr[EI_DATA] == ELFDATA2LSB)
+	if (header->e_ident[EI_DATA] == ELFDATA2LSB)
 		lendian = 1;
 	
 	return (EXIT_SUCCESS);
