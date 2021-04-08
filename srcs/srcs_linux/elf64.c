3ef44a0a069b45956a0845a88608da0b0473527a
aabelque <aabelque@student.42.fr>
2021-04-08 11:11:14 +0200

    test elf on linux

--- srcs/srcs_linux/elf64.c
+++ srcs/srcs_linux/elf64.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 19:22:26 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/07 18:15:17 by aabelque         ###   ########.fr       */
+/*   Updated: 2021/04/08 11:10:49 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,17 @@
 int			elf64(char *ptr, char *offset)
 {
 	short				lendian = 0;
-	Elf64_Ehdr			*header;
-	Elf64_Phdr			*program_hdr;
+	Elf64_Ehdr			*eh;
+	Elf64_Phdr			*ph;
 
-	header = (Elf64_Ehdr *)ptr;
-	if (header->e_ident[EI_DATA] == ELFDATA2LSB)
+	eh = (Elf64_Ehdr *)ptr;
+	if (eh->e_ident[EI_DATA] == ELFDATA2LSB)
 		lendian = 1;
-	for (int i = 0; i < header->e_shnum; i++)
+	for (int i = 0; i < eh->e_shnum; i++)
 	{
-		program_hdr = (Elf64_Phdr *)(void *)ptr + (header->e_phoff + header->e_phentsize * i);
-		ft_putnbr(swap64(program_hdr->p_type));
+		ph = (Elf64_Phdr *)(void *)ptr + (eh->e_phoff + eh->e_phentsize * i);
+		if (ph->p_type == PT_DYNAMIC)
+			ft_putnbr(swap64(ph->p_type));
 	}
 
 	
