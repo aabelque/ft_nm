ef4ae38887a28dfc97f27a59abaf46ed7e1def75
aabelque <aabelque@student.42.fr>
2021-04-09 13:17:30 +0200

    test elf on linux

--- srcs/srcs_linux/elf64.c
+++ srcs/srcs_linux/elf64.c
@@ -6,11 +6,31 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 19:22:26 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/08 11:43:09 by aabelque         ###   ########.fr       */
+/*   Updated: 2021/04/09 13:17:27 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_nm.h"
+
+static int	get_dynamic_table(Elf64_Dyn *dyn)
+{
+	int			i = 0;
+	const char	*stringtable = NULL;
+
+	while (42)
+	{
+		if (dyn[i].d_tag == DT_NULL)
+			break ;
+		if (dyn[i].d_tag == STRTAB)
+			stringtable = (const char *)dyn[i].d_un.d_ptr;
+		i++;
+	}
+	if (stringtable == NULL)
+		return (ft_perror("There is no string table\n", 0));
+	prints(stringtable);
+	write(1, "\n", 1);
+	return (EXIT_SUCCESS);
+}
 
 int			elf64(char *ptr, char *offset)
 {
@@ -25,17 +45,8 @@ int			elf64(char *ptr, char *offset)
 	{
 		ph = (Elf64_Phdr *)((char *)ptr + (eh->e_phoff + eh->e_phentsize * i));
 		if (ph->p_type == PT_DYNAMIC)
-		{
-			ft_putnbr(ph->p_type);
-			write(1, "\n", 1);
-		}
-		if (ph->p_type == PT_LOAD)
-		{
-			ft_putnbr(ph->p_type);
-			write(1, "\n", 1);
-		}
+			if (get_dynamic_table((Elf64_Dyn *)(ptr + ph->p_offset)))
+				return (EXIT_FAILURE);
 	}
-
-	
 	return (EXIT_SUCCESS);
 }
