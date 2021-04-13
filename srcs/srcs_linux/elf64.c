d23acc01d0b1819711b141c5d7254a2e27f04ff7
aabelque <aabelque@student.42.fr>
2021-04-13 11:16:00 +0200

    test elf on linux

--- srcs/srcs_linux/elf64.c
+++ srcs/srcs_linux/elf64.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 19:22:26 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/13 11:15:08 by aabelque         ###   ########.fr       */
+/*   Updated: 2021/04/13 11:15:57 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ int			elf64(char *ptr, char *offset)
 		if (sh[i].sh_type == SHT_SYMTAB)
 		{
 			sym = (Elf64_Sym *)sh;
-			hexdump(sym->st_value, 16, 16);
+			hexdump(swap64(sym->st_value), 16, 16);
 			prints("Symtab\n");
 		}
 		if (sh[i].sh_type == SHT_DYNAMIC)
