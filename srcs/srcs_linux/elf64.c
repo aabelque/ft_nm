fc8496895e5d082e0e0142515ba91b596a51b30c
aabelque <aabelque@student.42.fr>
2021-04-17 17:32:49 +0200

    test elf on linux

--- srcs/srcs_linux/elf64.c
+++ srcs/srcs_linux/elf64.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 19:22:26 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/17 17:32:31 by aabelque         ###   ########.fr       */
+/*   Updated: 2021/04/17 17:32:47 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 static inline char	get_flags(Elf64_Shdr *sh, t_symelf sym) {
 
-	if (sh[sym.st_shndx].sh_type == SHT_NOBITS
-			&& sh[sym.st_shndx].sh_flags == (SHF_ALLOC | SHF_WRITE))
+	if (sh[sym.shndx].sh_type == SHT_NOBITS
+			&& sh[sym.shndx].sh_flags == (SHF_ALLOC | SHF_WRITE))
 		return ('B');
 	return (' ');
 }
