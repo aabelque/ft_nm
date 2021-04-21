7a4d9ae3729f7e3db740aab6eecefb85d06f4830
aabelque <aabelque@student.42.fr>
2021-04-21 17:47:45 +0200

    test elf on linux

--- srcs/srcs_linux/elf64.c
+++ srcs/srcs_linux/elf64.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 19:22:26 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/21 17:42:12 by aabelque         ###   ########.fr       */
+/*   Updated: 2021/04/21 17:47:44 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,8 +101,8 @@ static inline int	print_symelf(Elf64_Sym *sym, Elf64_Shdr *sh, Elf64_Ehdr *eh, i
 		/* write(1, " ", 1); */
 		/* ft_putnbr(symbols[i].bind); */
 		write(1, " ", 1);
-		ft_putnbr(symbols[i].shndx);
-		write(1, " ", 1);
+		/* ft_putnbr(symbols[i].shndx); */
+		/* write(1, " ", 1); */
 		prints(symbols[i].name);
 		write(1, "\n", 1);
 	}
