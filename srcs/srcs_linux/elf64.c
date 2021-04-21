355fea6884f834cd17fcd98561c532acd836c444
aabelque <aabelque@student.42.fr>
2021-04-21 17:09:50 +0200

    test elf on linux

--- srcs/srcs_linux/elf64.c
+++ srcs/srcs_linux/elf64.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 19:22:26 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/21 17:06:20 by aabelque         ###   ########.fr       */
+/*   Updated: 2021/04/21 17:09:49 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,7 +96,7 @@ static t_elf_section	*get_elfsection(char *strtable, Elf64_Shdr *sh, int shnum) 
 	sections = malloc(sizeof(t_elf_section) * shnum);
 	if (!sections)
 		return (NULL);
-	for (i = 1; i < shnum; i++) {
+	for (i = 0; i < shnum; i++) {
 		sections[i].name = strtable + sh[i].sh_name;
 	}
 	return (sections);
