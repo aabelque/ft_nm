babf91ebf01d25251519af05e13d32d0bb42d095
aabelque <aabelque@student.42.fr>
2021-04-21 17:42:13 +0200

    test elf on linux

--- srcs/srcs_linux/elf64.c
+++ srcs/srcs_linux/elf64.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 19:22:26 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/21 17:41:03 by aabelque         ###   ########.fr       */
+/*   Updated: 2021/04/21 17:42:12 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,26 @@ static inline char	get_flags(Elf64_Shdr *sh, t_elf_symbol sym, t_elf_section *se
 		if (sym.shndx == SHN_UNDEF)
 			return (sym.bind == STB_WEAK ? 'w' : 'U');
 	}
-	else if (ft_strcmp(sections[sym.shndx].name, ".dynamic")
-			|| ft_strcmp(sections[sym.shndx].name, ".got")
-			|| ft_strcmp(sections[sym.shndx].name, ".data")
+	else if (!ft_strcmp(sections[sym.shndx].name, ".dynamic")
+			|| !ft_strcmp(sections[sym.shndx].name, ".got")
+			|| !ft_strcmp(sections[sym.shndx].name, ".data")
 			) {
 		if (sym.bind == STB_WEAK)
 			return (sym.shndx == SHN_UNDEF ? 'w' : 'W');
 		return (sym.bind == STB_LOCAL ? 'd' : 'D');
 	}
-	else if (ft_strcmp(sections[sym.shndx].name, ".rodata")
-			|| ft_strcmp(sections[sym.shndx].name, ".eh_frame")
-			|| ft_strcmp(sections[sym.shndx].name, ".eh_frame_hdr")
+	else if (!ft_strcmp(sections[sym.shndx].name, ".rodata")
+			|| !ft_strcmp(sections[sym.shndx].name, ".eh_frame")
+			|| !ft_strcmp(sections[sym.shndx].name, ".eh_frame_hdr")
 			)
 		return (sym.bind == STB_LOCAL ? 'r' : 'R');
-	else if (ft_strcmp(sections[sym.shndx].name, ".bss"))
+	else if (!ft_strcmp(sections[sym.shndx].name, ".bss"))
 		return (sym.bind == STB_LOCAL ? 'b' : 'B');
-	else if (ft_strcmp(sections[sym.shndx].name, ".text")
-			|| ft_strcmp(sections[sym.shndx].name, ".fini_array")
-			|| ft_strcmp(sections[sym.shndx].name, ".fini")
-			|| ft_strcmp(sections[sym.shndx].name, ".init")
-			|| ft_strcmp(sections[sym.shndx].name, ".init_array")
+	else if (!ft_strcmp(sections[sym.shndx].name, ".text")
+			|| !ft_strcmp(sections[sym.shndx].name, ".fini_array")
+			|| !ft_strcmp(sections[sym.shndx].name, ".fini")
+			|| !ft_strcmp(sections[sym.shndx].name, ".init")
+			|| !ft_strcmp(sections[sym.shndx].name, ".init_array")
 			)
 		return (sym.bind == STB_LOCAL ? 't' : 'T');
 	/* if (sh[sym.shndx].sh_type == SHT_NOBITS */
