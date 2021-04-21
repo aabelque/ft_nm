cfb2bba73aacc9ca827bccdff10b188aed9a21ee
aabelque <aabelque@student.42.fr>
2021-04-21 13:34:44 +0200

    test elf on linux

--- srcs/srcs_linux/elf64.c
+++ srcs/srcs_linux/elf64.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 19:22:26 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/21 13:33:39 by aabelque         ###   ########.fr       */
+/*   Updated: 2021/04/21 13:34:40 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,10 +60,6 @@ static inline int	print_symelf(Elf64_Sym *sym, Elf64_Shdr *sh, Elf64_Ehdr *eh, i
 			symbols[j].name = symstr_table + sym[i].st_name;
 			symbols[j].shndx = sym[i].st_shndx;
 			symbols[j].value = sym[i].st_value;
-			ft_putnbr(symbols[j].shndx);
-			write(1, " ", 1);
-			prints(symbols[j].name);
-			write(1, "\n", 1);
 			j++;
 		}
 	}
@@ -101,10 +97,10 @@ int			elf64(char *ptr, char *offset) {
 	ft_putnbr(eh->e_type);
 	write(1, "\n", 1);
 	for (int i = 0; i < eh->e_shnum; i++) {
-		/* prints(names + sh[i].sh_name); */
-		/* write(1, " ", 1); */
-		/* ft_putnbr(i); */
-		/* write(1, "\n", 1); */
+		prints(names + sh[i].sh_name);
+		write(1, " ", 1);
+		ft_putnbr(i);
+		write(1, "\n", 1);
 		if (sh[i].sh_type == SHT_SYMTAB
 				|| sh[i].sh_type == SHT_DYNSYM)
 			if (print_symelf((Elf64_Sym *)((char *)eh + sh[i].sh_offset), sh, eh, i))
