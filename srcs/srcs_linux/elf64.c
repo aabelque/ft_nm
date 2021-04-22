67c2709431926cf4ddd7e05c49064bfdf08511fc
aabelque <aabelque@student.42.fr>
2021-04-22 23:50:53 +0200

    test elf on linux

--- srcs/srcs_linux/elf64.c
+++ srcs/srcs_linux/elf64.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 19:22:26 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/22 23:47:18 by aabelque         ###   ########.fr       */
+/*   Updated: 2021/04/22 23:50:50 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -130,7 +130,7 @@ int			elf64(char *ptr, char *offset) {
 	sh = (Elf64_Shdr *)(ptr + eh->e_shoff);
 	ft_putnbr(lendian);
 	write(1, "\n", 1);
-	ft_putnbr(eh->e_shnum);
+	ft_putnbr(swap64(eh->e_shnum));
 	write(1, "\n", 1);
 	strtable = ptr + sh[eh->e_shstrndx].sh_offset;
 	if (!(sections = get_elfsection(strtable, sh, eh->e_shnum)))
