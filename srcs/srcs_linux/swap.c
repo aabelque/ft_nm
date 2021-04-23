a800dadc693a0b7bb0c15c6014c1da41f0348f99
aabelque <aabelque@student.42.fr>
2021-04-23 12:05:16 +0200

    test elf on linux

--- srcs/srcs_linux/swap.c
+++ srcs/srcs_linux/swap.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 20:00:44 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/23 10:50:22 by aabelque         ###   ########.fr       */
+/*   Updated: 2021/04/23 12:05:14 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,8 +59,18 @@ uint64_t			swap64(uint64_t x)
 
 uint64_t			swap64_little_to_big(uint64_t x)
 {
-	x = (x << 32) | (x >> 32);
-    x = ((x << 16) & 0xFFFF0000FFFF0000ULL ) | ((x >> 16) & 0x0000FFFF0000FFFFULL );
-    return (((x << 8) & 0xFF00FF00FF00FF00ULL ) | ((x >> 8) & 0x00FF00FF00FF00FFULL ));
+	uint64_t	n0, n1, n2, n3;
+	uint64_t	res;
+
+	n0 = (x & 0x000000ff) << 24;
+	n1 = (x & 0x0000ff00) << 8;
+	n2 = (x & 0x00ff0000) >> 8;
+	n3 = (x & 0xff000000) >> 24;
+	res = n0 | n1 | n2 | n3;
+
+	return (res);
+	/* x = (x << 32) | (x >> 32); */
+    /* x = ((x << 16) & 0xFFFF0000FFFF0000ULL ) | ((x >> 16) & 0x0000FFFF0000FFFFULL ); */
+    /* return (((x << 8) & 0xFF00FF00FF00FF00ULL ) | ((x >> 8) & 0x00FF00FF00FF00FFULL )); */
     /* return (x << 32) | (x >> 32); */
 }
