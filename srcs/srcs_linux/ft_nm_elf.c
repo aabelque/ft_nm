06a7a61605e87f95b142807169ac6746efdefbf2
aabelque <aabelque@student.42.fr>
2021-04-23 16:45:33 +0200

    test elf on linux

--- srcs/srcs_linux/ft_nm_elf.c
+++ srcs/srcs_linux/ft_nm_elf.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 14:33:02 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/23 16:44:43 by aabelque         ###   ########.fr       */
+/*   Updated: 2021/04/23 16:45:28 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,12 +40,8 @@ int			main(int ac, char **av) {
 		opt = 1;
 	i = opt ? 1 : 0;
 	while (++i < ac) {
-		if (opt) {
-			if (ac > 2 && i == 2)
-				write(1, "\n", 1);
-		}
-		/* else if ((ac > 2 && i == 1)) */
-		/* 	write(1, "\n", 1); */
+		if ((ac > 2 && i == 1))
+			write(1, "\n", 1);
 		if (ac > 2) {
 			prints(av[i]);
 			write(1, ":\n", 2);
