951dfe306ea8edc9146df15797b54b82c5268c6b
aabelque <aabelque@student.42.fr>
2021-04-25 14:51:13 +0200

    test elf on linux

--- srcs/srcs_linux/elf64.c
+++ srcs/srcs_linux/elf64.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 19:22:26 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/25 14:50:06 by aabelque         ###   ########.fr       */
+/*   Updated: 2021/04/25 14:51:11 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -162,6 +162,6 @@ int			elf64(char *ptr, char *offset, int opt) {
 	}
 	if (no_sym)
 		return (ft_perror("No symbols\n", 0));
-	free(sections);
+	/* free(sections); */
 	return (EXIT_SUCCESS);
 }
