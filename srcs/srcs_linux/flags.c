b9523db41662154530b23c139f26bc4c93ec33dd
aabelque <aabelque@student.42.fr>
2021-04-25 18:49:25 +0200

    test elf on linux

--- srcs/srcs_linux/flags.c
+++ srcs/srcs_linux/flags.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 15:24:05 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/25 18:21:05 by aabelque         ###   ########.fr       */
+/*   Updated: 2021/04/25 18:49:23 by aabelque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,10 @@ int			rodata_flags(t_elf_symbol sym, t_elf_section *sections) {
 
 char			get_flags(t_elf_symbol sym, t_elf_section *sections) {
 
-	if (sym.shndx > MAX_SECTIONS)
+	if (sym.shndx == SHN_ABS)
 		return (sym.bind == STB_LOCAL ? 'a' : 'A');
+	/* if (sym.shndx > MAX_SECTIONS) */
+	/* 	return (sym.bind == STB_LOCAL ? 'a' : 'A'); */
 	prints(sections[sym.shndx].name);
 	prints(" -->  ");
 	if (sections[sym.shndx].name == NULL) {
