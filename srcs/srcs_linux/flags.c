178616fdf1c00610e6f4374c0050980323f5383c
aabelque <aabelque@student.42.fr>
2021-04-26 17:47:12 +0200

    test elf on linux

--- srcs/srcs_linux/flags.c
+++ srcs/srcs_linux/flags.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 15:24:05 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/26 17:46:26 by azziz            ###   ########.fr       */
+/*   Updated: 2021/04/26 17:47:10 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,10 +60,10 @@ char			get_flags(t_elf_symbol sym, t_elf_section *sections) {
 		prints("flag: ");
 		ft_putnbr(sections[sym.shndx].flag);
 		prints(" ");
-		ft_putnbr(sections[sym.shndx].flag | (SHF_ALLOC | SHF_WRITE) & SHF_MASKPROC);
+		ft_putnbr(sections[sym.shndx].flag | (SHF_ALLOC | SHF_WRITE));
 		prints(" ");
-		/* if (sections[sym.shndx].flag == sections[sym.shndx].flag | (SHF_ALLOC | SHF_WRITE) & SHF_MASKPROC) */
-		/* 	return (sym.bind == STB_LOCAL ? 'g' : 'G'); */
+		if (sections[sym.shndx].flag == sections[sym.shndx].flag | (SHF_ALLOC | SHF_WRITE))
+			return (sym.bind == STB_LOCAL ? 'g' : 'G');
 		if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_EXECINSTR))
 			return (sym.bind == STB_LOCAL ? 't' : 'T');
 		else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE)) {
