7315c9b5323d05f539d43335632af734e784e8be
aabelque <aabelque@student.42.fr>
2021-04-26 17:37:23 +0200

    test elf on linux

--- srcs/srcs_linux/flags.c
+++ srcs/srcs_linux/flags.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 15:24:05 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/26 17:36:28 by azziz            ###   ########.fr       */
+/*   Updated: 2021/04/26 17:37:21 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,11 +88,11 @@ char			get_flags(t_elf_symbol sym, t_elf_section *sections) {
 	/* } */
 	else if (sections[sym.shndx].type == SHT_NOBITS) {
 		prints("flag: ");
-		ft_putnbr(sections[sym.shndx].flag & SHF_MASKPROC);
+		ft_putnbr(sections[sym.shndx].flag);
 		write(1, " ", 1);
 		ft_putnbr(sections[sym.shndx].flag | (SHF_ALLOC | SHF_WRITE) & SHF_MASKPROC);
 		write(1, " ", 1);
-		if ((sections[sym.shndx].flag & SHF_MASKPROC) == (SHF_ALLOC | SHF_WRITE))
+		if (sections[sym.shndx].flag == sections[sym.shndx].flag | (SHF_ALLOC | SHF_WRITE) & SHF_MASKPROC)
 			return (sym.bind == STB_LOCAL ? 's' : 'S');
 		else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE)) {
 			if (sym.bind == STB_WEAK && sym.type == STT_OBJECT)
