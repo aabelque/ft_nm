6af37415e2c5189c45ef9d8b1a90922e73ed66fb
aabelque <aabelque@student.42.fr>
2021-04-26 17:38:55 +0200

    test elf on linux

--- srcs/srcs_linux/flags.c
+++ srcs/srcs_linux/flags.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 15:24:05 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/26 17:37:21 by azziz            ###   ########.fr       */
+/*   Updated: 2021/04/26 17:38:48 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,9 @@ char			get_flags(t_elf_symbol sym, t_elf_section *sections) {
 			return (sym.bind == STB_WEAK ? 'w' : 'U');
 	}
 	else if (sections[sym.shndx].type == SHT_PROGBITS) {
-		if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_EXECINSTR))
+		if (sections[sym.shndx].flag == sections[sym.shndx].flag | (SHF_ALLOC | SHF_WRITE) & SHF_MASKPROC)
+			return (sym.bind == STB_LOCAL ? 'g' : 'G');
+		else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_EXECINSTR))
 			return (sym.bind == STB_LOCAL ? 't' : 'T');
 		else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE)) {
 			if (sym.bind == STB_WEAK)
@@ -87,11 +89,6 @@ char			get_flags(t_elf_symbol sym, t_elf_section *sections) {
 	/* 	return (sym.bind == STB_LOCAL ? 'r' : 'R'); */
 	/* } */
 	else if (sections[sym.shndx].type == SHT_NOBITS) {
-		prints("flag: ");
-		ft_putnbr(sections[sym.shndx].flag);
-		write(1, " ", 1);
-		ft_putnbr(sections[sym.shndx].flag | (SHF_ALLOC | SHF_WRITE) & SHF_MASKPROC);
-		write(1, " ", 1);
 		if (sections[sym.shndx].flag == sections[sym.shndx].flag | (SHF_ALLOC | SHF_WRITE) & SHF_MASKPROC)
 			return (sym.bind == STB_LOCAL ? 's' : 'S');
 		else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE)) {
@@ -100,9 +97,5 @@ char			get_flags(t_elf_symbol sym, t_elf_section *sections) {
 			return (sym.bind == STB_LOCAL ? 'b' : 'B');
 		}
 	}
-	else if (!ft_strcmp(sections[sym.shndx].name, ".sdata"))
-		return (sym.bind == STB_LOCAL ? 'g' : 'G');
-	else if (!ft_strcmp(sections[sym.shndx].name, ".IA_64.unwind_hdr"))
-		return (0);
 	return ('?');
 }
