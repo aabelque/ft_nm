8b01673f49c67c75642d03330a5229568699d859
aabelque <aabelque@student.42.fr>
2021-04-27 16:00:11 +0200

    test elf on linux

--- srcs/srcs_linux/flags.c
+++ srcs/srcs_linux/flags.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 15:24:05 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/27 15:59:39 by azziz            ###   ########.fr       */
+/*   Updated: 2021/04/27 16:00:02 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,8 +45,8 @@ char			get_flags(t_elf_symbol sym, t_elf_section *sections) {
 
 	if (sym.shndx > MAX_SECTIONS || sym.shndx == SHN_ABS)
 		return (sym.bind == STB_LOCAL ? 'a' : 'A');
-	prints(sections[sym.shndx].name);
-	prints(" -->  ");
+	/* prints(sections[sym.shndx].name); */
+	/* prints(" -->  "); */
 	/* ft_putnbr(sections[sym.shndx].flag); */
 	/* prints(" "); */
 	/* ft_putnbr((sections[sym.shndx].flag & SHF_MASKOS) | SHF_ALLOC); */
