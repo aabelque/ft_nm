1bcf95c735abe42f5ee8a6494ffbcc08a9a89b20
aabelque <aabelque@student.42.fr>
2021-04-27 15:46:26 +0200

    test elf on linux

--- srcs/srcs_linux/flags.c
+++ srcs/srcs_linux/flags.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 15:24:05 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/27 15:45:56 by azziz            ###   ########.fr       */
+/*   Updated: 2021/04/27 15:46:25 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,11 +76,11 @@ char			get_flags(t_elf_symbol sym, t_elf_section *sections) {
 			}
 			return (sym.bind == STB_LOCAL ? 'd' : 'D');
 		}
-		/* else if (sections[sym.shndx].flag == SHF_ALLOC) */ 
-		/* 	return (sym.bind == STB_LOCAL ? 'r' : 'R'); */
+		else if (sections[sym.shndx].flag == SHF_ALLOC) 
+			return (sym.bind == STB_LOCAL ? 'r' : 'R');
 		else if (sections[sym.shndx].flag == ((sections[sym.shndx].flag & SHF_MASKOS) | SHF_ALLOC))
 			return (sym.bind == STB_LOCAL ? 'r' : 'R');
-		else if (sections[sym.shndx].flag == (sections[sym.shndx].flag & SHF_MASKPROC) | (SHF_ALLOC | SHF_WRITE))
+		else if (sections[sym.shndx].flag == ((sections[sym.shndx].flag & SHF_MASKPROC) | (SHF_ALLOC | SHF_WRITE)))
 			return (sym.bind == STB_LOCAL ? 'g' : 'G');
 	}
 	else if (sections[sym.shndx].type == SHT_DYNAMIC) {
