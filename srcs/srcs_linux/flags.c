2fad6d28e4c362eb673deed3c773057977f5e0f4
aabelque <aabelque@student.42.fr>
2021-04-28 13:22:37 +0200

    test elf on linux

--- srcs/srcs_linux/flags.c
+++ srcs/srcs_linux/flags.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 15:24:05 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/28 13:17:51 by azziz            ###   ########.fr       */
+/*   Updated: 2021/04/28 13:22:35 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,10 @@ char			get_flags_ppc(t_elf_symbol sym, t_elf_section *sections) {
 	else if (sections[sym.shndx].type == SHT_NOBITS) {
 		if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE)) {
 			if (sym.type == STT_OBJECT)
-				return (sym.bind == STB_LOCAL ? 'V' : (sym.bind == STB_GLOBAL) ? 'S' : 'V');
+				if (sym.bind == STB_GLOBAL) {
+					if (!ft_strcmp(sections[sym.shndx].name, ".sbss"))
+						return (sym.bind == STB_LOCAL ? 'V' : (sym.bind == STB_GLOBAL) ? 'S' : 'V');
+					return ('B');
 			return (sym.bind == STB_LOCAL ? 'b' : 'B');
 		}
 		else if (sections[sym.shndx].flag == (sections[sym.shndx].flag & SHF_MASKPROC) | (SHF_ALLOC | SHF_WRITE))
