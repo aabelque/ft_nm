e34902034d45ffd910333a0a64a1f9136806a814
aabelque <aabelque@student.42.fr>
2021-04-28 13:34:39 +0200

    test elf on linux

--- srcs/srcs_linux/elf32.c
+++ srcs/srcs_linux/elf32.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 16:49:57 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/28 13:25:55 by azziz            ###   ########.fr       */
+/*   Updated: 2021/04/28 13:34:11 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,12 +36,12 @@ static inline void			print_symbols(t_elf_symbol symbols, t_elf_section *sections
 		hexdump(symbols.value, 16, 8);
 	write(1, " ", 1);
 	write(1, &c, 1);
-	/* write(1, " ", 1); */
-	/* ft_putnbr(symbols.bind); */
-	/* write(1, " ", 1); */
-	/* ft_putnbr(symbols.type); */
-	/* write(1, " ", 1); */
-	/* ft_putnbr(symbols.shndx); */
+	write(1, " ", 1);
+	ft_putnbr(symbols.bind);
+	write(1, " ", 1);
+	ft_putnbr(symbols.type);
+	write(1, " ", 1);
+	ft_putnbr(symbols.shndx);
 	write(1, " ", 1);
 	prints(symbols.name);
 	write(1, "\n", 1);
--- srcs/srcs_linux/flags.c
+++ srcs/srcs_linux/flags.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 15:24:05 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/28 13:25:50 by azziz            ###   ########.fr       */
+/*   Updated: 2021/04/28 13:34:38 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ char			get_flags_ppc(t_elf_symbol sym, t_elf_section *sections) {
 
 	if (sym.shndx > MAX_SECTIONS || sym.shndx == SHN_ABS)
 		return (sym.bind == STB_LOCAL ? 'a' : 'A');
-	/* prints(sections[sym.shndx].name); */
-	/* prints(" -->  "); */
+	prints(sections[sym.shndx].name);
+	prints(" -->  ");
 	/* ft_putnbr(sections[sym.shndx].flag); */
 	/* prints(" "); */
 	/* ft_putnbr(SHF_ALLOC | SHF_WRITE); */
@@ -37,68 +37,68 @@ char			get_flags_ppc(t_elf_symbol sym, t_elf_section *sections) {
 		if (sym.shndx == SHN_UNDEF)
 			return (sym.bind == STB_WEAK ? 'w' : 'U');
 	}
-	else if (sections[sym.shndx].type == SHT_PROGBITS) {
-		if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_EXECINSTR)) {
-			if (sym.bind == STB_WEAK)
-				return (sym.shndx == SHN_UNDEF ? 'w' : 'W');
-			return (sym.bind == STB_LOCAL ? 't' : 'T');
-		}
-		else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE)) {
-			if (sym.bind == STB_WEAK) {
-				if (sym.type == STT_OBJECT)
-					return (sym.shndx == SHN_UNDEF ? 'v' : 'V');
-				return (sym.shndx == SHN_UNDEF ? 'w' : 'W');
-			}
-			else if (sym.bind == STB_GLOBAL) {
-				if (sym.type == STT_OBJECT)
-					return ('D');
-			}
-			return (sym.bind == STB_LOCAL ? 'd' : 'D');
-		}
-		else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE | SHF_EXECINSTR))
-			return (sym.bind == STB_LOCAL ? 't' : 'T');
-		else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_MERGE))
-			return (sym.bind == STB_LOCAL ? 'r' : 'R');
-		else if (sections[sym.shndx].flag == SHF_ALLOC) {
-			if (sym.bind == STB_WEAK) {
-				if (sym.type == STT_OBJECT)
-					return (sym.shndx == SHN_UNDEF ? 'v' : 'V');
-			}
-			return (sym.bind == STB_LOCAL ? 'r' : 'R');
-		}
-		else if (sections[sym.shndx].flag == ((sections[sym.shndx].flag & SHF_MASKOS) | SHF_ALLOC))
-			return (sym.bind == STB_LOCAL ? 'r' : 'R');
-		else if (sections[sym.shndx].flag == ((sections[sym.shndx].flag & SHF_MASKPROC) | (SHF_ALLOC | SHF_WRITE)))
-			return (sym.bind == STB_LOCAL ? 'g' : 'G');
-	}
-	else if (sections[sym.shndx].type == SHT_DYNAMIC) {
-		if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE))
-			return (sym.bind == STB_LOCAL ? 'd' : 'D');
-		if (sections[sym.shndx].flag == SHF_ALLOC)
-			return (sym.bind == STB_LOCAL ? 'r' : 'R');
-	}
-	else if (sections[sym.shndx].type == SHT_INIT_ARRAY
-			|| sections[sym.shndx].type == SHT_FINI_ARRAY) {
-		if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE))
-			return (sym.bind == STB_LOCAL ? 't' : 'T');
-	}
-	else if (sections[sym.shndx].type == SHT_NOBITS) {
-		if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE)) {
-			if (sym.type == STT_OBJECT)
-				if (sym.bind == STB_GLOBAL) {
-					if (!ft_strcmp(sections[sym.shndx].name, ".sbss"))
-						return (sym.bind == STB_LOCAL ? 'V' : (sym.bind == STB_GLOBAL) ? 'S' : 'V');
-					return ('B');
-				}
-			return (sym.bind == STB_LOCAL ? 'b' : 'B');
-		}
-		else if (sections[sym.shndx].flag == (sections[sym.shndx].flag & SHF_MASKPROC) | (SHF_ALLOC | SHF_WRITE))
-			if (sym.type == STT_OBJECT)
-				return (sym.bind == STB_LOCAL ? 'B' : 'S');
-			return (sym.bind == STB_LOCAL ? 's' : 'S');
-	}
-	else if (sections[sym.shndx].type == SHT_NOTE)
-		return (sym.bind == STB_LOCAL ? 'r' : 'R');
+	/* else if (sections[sym.shndx].type == SHT_PROGBITS) { */
+	/* 	if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_EXECINSTR)) { */
+	/* 		if (sym.bind == STB_WEAK) */
+	/* 			return (sym.shndx == SHN_UNDEF ? 'w' : 'W'); */
+	/* 		return (sym.bind == STB_LOCAL ? 't' : 'T'); */
+	/* 	} */
+	/* 	else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE)) { */
+	/* 		if (sym.bind == STB_WEAK) { */
+	/* 			if (sym.type == STT_OBJECT) */
+	/* 				return (sym.shndx == SHN_UNDEF ? 'v' : 'V'); */
+	/* 			return (sym.shndx == SHN_UNDEF ? 'w' : 'W'); */
+	/* 		} */
+	/* 		else if (sym.bind == STB_GLOBAL) { */
+	/* 			if (sym.type == STT_OBJECT) */
+	/* 				return ('D'); */
+	/* 		} */
+	/* 		return (sym.bind == STB_LOCAL ? 'd' : 'D'); */
+	/* 	} */
+	/* 	else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE | SHF_EXECINSTR)) */
+	/* 		return (sym.bind == STB_LOCAL ? 't' : 'T'); */
+	/* 	else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_MERGE)) */
+	/* 		return (sym.bind == STB_LOCAL ? 'r' : 'R'); */
+	/* 	else if (sections[sym.shndx].flag == SHF_ALLOC) { */
+	/* 		if (sym.bind == STB_WEAK) { */
+	/* 			if (sym.type == STT_OBJECT) */
+	/* 				return (sym.shndx == SHN_UNDEF ? 'v' : 'V'); */
+	/* 		} */
+	/* 		return (sym.bind == STB_LOCAL ? 'r' : 'R'); */
+	/* 	} */
+	/* 	else if (sections[sym.shndx].flag == ((sections[sym.shndx].flag & SHF_MASKOS) | SHF_ALLOC)) */
+	/* 		return (sym.bind == STB_LOCAL ? 'r' : 'R'); */
+	/* 	else if (sections[sym.shndx].flag == ((sections[sym.shndx].flag & SHF_MASKPROC) | (SHF_ALLOC | SHF_WRITE))) */
+	/* 		return (sym.bind == STB_LOCAL ? 'g' : 'G'); */
+	/* } */
+	/* else if (sections[sym.shndx].type == SHT_DYNAMIC) { */
+	/* 	if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE)) */
+	/* 		return (sym.bind == STB_LOCAL ? 'd' : 'D'); */
+	/* 	if (sections[sym.shndx].flag == SHF_ALLOC) */
+	/* 		return (sym.bind == STB_LOCAL ? 'r' : 'R'); */
+	/* } */
+	/* else if (sections[sym.shndx].type == SHT_INIT_ARRAY */
+	/* 		|| sections[sym.shndx].type == SHT_FINI_ARRAY) { */
+	/* 	if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE)) */
+	/* 		return (sym.bind == STB_LOCAL ? 't' : 'T'); */
+	/* } */
+	/* else if (sections[sym.shndx].type == SHT_NOBITS) { */
+	/* 	if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE)) { */
+	/* 		if (sym.type == STT_OBJECT) */
+	/* 			if (sym.bind == STB_GLOBAL) { */
+	/* 				if (!ft_strcmp(sections[sym.shndx].name, ".sbss")) */
+	/* 					return (sym.bind == STB_LOCAL ? 'V' : (sym.bind == STB_GLOBAL) ? 'S' : 'V'); */
+	/* 				return ('B'); */
+	/* 			} */
+	/* 		return (sym.bind == STB_LOCAL ? 'b' : 'B'); */
+	/* 	} */
+	/* 	else if (sections[sym.shndx].flag == (sections[sym.shndx].flag & SHF_MASKPROC) | (SHF_ALLOC | SHF_WRITE)) */
+	/* 		if (sym.type == STT_OBJECT) */
+	/* 			return (sym.bind == STB_LOCAL ? 'B' : 'S'); */
+	/* 		return (sym.bind == STB_LOCAL ? 's' : 'S'); */
+	/* } */
+	/* else if (sections[sym.shndx].type == SHT_NOTE) */
+	/* 	return (sym.bind == STB_LOCAL ? 'r' : 'R'); */
 	return ('?');
 }
 
