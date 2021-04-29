e8aa11c0eb16eaaa0d6c47f9e03a9f987564fe99
aabelque <aabelque@student.42.fr>
2021-04-29 14:20:52 +0200

    test elf on linux

--- srcs/srcs_linux/libc.c
+++ srcs/srcs_linux/libc.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 09:40:00 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/29 14:20:03 by azziz            ###   ########.fr       */
+/*   Updated: 2021/04/29 14:20:50 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,24 +47,24 @@ int				ft_atoi(const char *str)
 int				ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t i = 0;
-	while ((s1[i] || s2[i]) && i < n)
-	{
-		if (s1[i] != s2[i])
-		{
-			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
-		}
-		i++;
-	}
-	return (0);
-	/* while (*s1 == *s2 && i < n) */
+	/* while ((s1[i] || s2[i]) && i < n) */
 	/* { */
-	/* 	if (*s1 == '\0') */
-	/* 		return (0); */
-	/* 	++s1; */
-	/* 	++s2; */
+	/* 	if (s1[i] != s2[i]) */
+	/* 	{ */
+	/* 		return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]); */
+	/* 	} */
 	/* 	i++; */
 	/* } */
-	/* return (*(unsigned char *)s1 - *(unsigned char *)s2); */
+	/* return (0); */
+	while ((*s1 == *s2) && i < n)
+	{
+		if (*s1 == '\0')
+			return (0);
+		++s1;
+		++s2;
+		i++;
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
 
 int				ft_strcmp(const char *s1, const char *s2)
