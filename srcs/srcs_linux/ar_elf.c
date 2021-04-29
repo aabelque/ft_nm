a6cf55110eef5a15e5623bf0e1455f1502fca5f6
aabelque <aabelque@student.42.fr>
2021-04-29 15:44:37 +0200

    test elf on linux

--- srcs/srcs_linux/ar_elf.c
+++ srcs/srcs_linux/ar_elf.c
@@ -6,7 +6,7 @@
 /*   By: azziz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 14:42:59 by azziz             #+#    #+#             */
-/*   Updated: 2021/04/29 15:43:41 by azziz            ###   ########.fr       */
+/*   Updated: 2021/04/29 15:44:36 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,11 @@ int		ar_elf(char *ptr, char *offset, char *bin, int opt)
 
 	ptr += SARMAG;
 	ar = (struct ar_hdr *)ptr;
-	prints(ar->ar_size);
-	ft_putnbr(ft_atoi(ar->ar_size));
-	ft_putnbr(sizeof(*ar));
+	ft_putnbr((int)&ptr);
 	printc('\n');
 	ptr += ft_atoi(ar->ar_size) + sizeof(*ar);
+	ft_putnbr((int)&ptr);
+	printc('\n');
 	if (check_offset_elf(ptr, offset))
 		return (ft_perror("Corrupted file\n", 0));
 	while (ptr < offset)
