a31c9a543ccac060688db7be1054bec65b9e33a5
aabelque <aabelque@student.42.fr>
2021-04-29 13:42:10 +0200

    test elf on linux

--- srcs/srcs_linux/ft_nm_elf.c
+++ srcs/srcs_linux/ft_nm_elf.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 14:33:02 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/29 13:41:47 by azziz            ###   ########.fr       */
+/*   Updated: 2021/04/29 13:42:08 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static int		nm_elf(char *ptr, char *offset, char *bin, int opt) {
 	if (check_offset_elf(ptr, offset))
 		return (ft_perror("Corrupted file\n", 0));
-	if (!ft_strcmp((char *)ptr[ARMAG], ARMAG))
+	if (!ft_strcmp(&ptr[ARMAG], ARMAG))
 		prints("It's an archive file\n");
 	if ((unsigned char)ptr[EI_MAG0] == 0x7f &&
 			(unsigned char)ptr[EI_MAG1] == 'E' &&
