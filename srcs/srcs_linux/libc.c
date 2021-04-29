386f0f4215698508485b5f9eacae180608fbb671
aabelque <aabelque@student.42.fr>
2021-04-29 14:22:23 +0200

    test elf on linux

--- srcs/srcs_linux/libc.c
+++ srcs/srcs_linux/libc.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 09:40:00 by aabelque          #+#    #+#             */
-/*   Updated: 2021/04/29 14:21:50 by azziz            ###   ########.fr       */
+/*   Updated: 2021/04/29 14:22:22 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,26 +46,14 @@ int				ft_atoi(const char *str)
 
 int				ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t i = 0;
-	/* while ((s1[i] || s2[i]) && i < n) */
-	/* { */
-	/* 	if (s1[i] != s2[i]) */
-	/* 	{ */
-	/* 		return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]); */
-	/* 	} */
-	/* 	i++; */
-	/* } */
-	/* return (0); */
-	while ((*s1 || *s2) && i < n)
+	while ((*s1 || *s2) && n--)
 	{
 		if (*s1 != *s2)
 			return (*(unsigned char *)s1 - *(unsigned char *)s2);
 		++s1;
 		++s2;
-		i++;
 	}
 	return (0);
-	/* return (*(unsigned char *)s1 - *(unsigned char *)s2); */
 }
 
 int				ft_strcmp(const char *s1, const char *s2)
