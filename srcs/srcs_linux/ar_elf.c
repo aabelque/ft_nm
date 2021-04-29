7129ac29c9d053d85419b48d48717cc35672c45c
aabelque <aabelque@student.42.fr>
2021-04-29 19:33:28 +0200

    test elf on linux

--- srcs/srcs_linux/ar_elf.c
+++ srcs/srcs_linux/ar_elf.c
@@ -6,7 +6,7 @@
 /*   By: azziz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 14:42:59 by azziz             #+#    #+#             */
-/*   Updated: 2021/04/29 19:29:55 by azziz            ###   ########.fr       */
+/*   Updated: 2021/04/29 19:33:26 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,13 @@ int		ar_elf(char *ptr, char *offset, char *bin, int opt)
 	ptr += ft_atoi(ar->ar_size) + sizeof(*ar);
 	if (check_offset_elf(ptr, offset))
 		return (ft_perror("Corrupted file\n", 0));
+	char s[16];
 	while (ptr < offset)
 	{
-		prints(ar->ar_name);
+		while (ar->ar_name[i] != '/') {
+			s[i] = ar->ar_name[i];
+			i++;
+		}
 		ar = (struct ar_hdr *)ptr;
 		prints(ar->ar_name);
 		if (ar->ar_size <= 0)
@@ -44,7 +48,7 @@ int		ar_elf(char *ptr, char *offset, char *bin, int opt)
 		len = ft_strlen(str);
 		while (!str[len++])
 			;
-		print_archive(str, bin);
+		print_archive(s, bin);
 		if (nm_elf(ptr + sizeof(struct ar_hdr) + (len - 1), offset, bin, opt))
 			return (ft_perror("Corrupted\n", 0));
 		ptr += ft_atoi(ar->ar_size) + sizeof(struct ar_hdr);
