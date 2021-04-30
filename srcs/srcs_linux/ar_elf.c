d1f2ec43df112eb68521537d416f8abc7850cef4
aabelque <aabelque@student.42.fr>
2021-04-30 13:34:13 +0200

    test elf on linux

--- srcs/srcs_linux/ar_elf.c
+++ srcs/srcs_linux/ar_elf.c
@@ -6,7 +6,7 @@
 /*   By: azziz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 14:42:59 by azziz             #+#    #+#             */
-/*   Updated: 2021/04/30 13:33:13 by azziz            ###   ########.fr       */
+/*   Updated: 2021/04/30 13:34:09 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@ int		ar_elf(char *ptr, char *offset, char *bin, int opt)
 	{
 		ar = (struct ar_hdr *)ptr;
 		prints(ptr);
+		return (0);
 		if (ar->ar_size <= 0)
 			return (EXIT_FAILURE);
 		str = ptr + sizeof(struct ar_hdr);
