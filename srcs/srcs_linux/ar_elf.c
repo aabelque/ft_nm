2c0f723967e0c6c609192c4108938f24cea86caf
aabelque <aabelque@student.42.fr>
2021-05-01 13:47:49 +0200

    test elf on linux

--- srcs/srcs_linux/ar_elf.c
+++ srcs/srcs_linux/ar_elf.c
@@ -6,7 +6,7 @@
 /*   By: azziz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 14:42:59 by azziz             #+#    #+#             */
-/*   Updated: 2021/05/01 13:46:53 by azziz            ###   ########.fr       */
+/*   Updated: 2021/05/01 13:47:48 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ int		ar_elf(char *ptr, char *offset, char *bin, int opt)
 	char			*str;
 	int				i = 0;
 
-	/* ptr += SARMAG; */
-	ar = (struct ar_hdr *)&ptr[SARMAG];
+	ptr += SARMAG;
+	ar = (struct ar_hdr *)ptr;
 	prints(ptr);
 	prints("\n ----------------------------- \n");
 	ptr += ft_atoi(ar->ar_size) + sizeof(*ar);
