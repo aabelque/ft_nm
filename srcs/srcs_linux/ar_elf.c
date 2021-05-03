52e5f129754fe7277fee40a98526fa6539ab5ab6
aabelque <aabelque@student.42.fr>
2021-05-03 13:26:27 +0200

    test elf on linux

--- srcs/srcs_linux/ar_elf.c
+++ srcs/srcs_linux/ar_elf.c
@@ -6,7 +6,7 @@
 /*   By: azziz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 14:42:59 by azziz             #+#    #+#             */
-/*   Updated: 2021/05/03 13:25:26 by azziz            ###   ########.fr       */
+/*   Updated: 2021/05/03 13:26:25 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,13 +94,10 @@ int				ar_elf(char *ptr, char *offset, char *bin, int opt)
 			/* } */
 		/* } */
 		/* printc('\n'); */
-		if (name)
-			print_archive(name);
-		else
-			print_archive(ar->ar_name);
+		(name) ? print_archive(name) : print_archive(ar->ar_name);
 		ar = (struct ar_hdr *)ptr;
-		/* if (nm_elf(ptr + sizeof(*ar), offset, bin, opt)) */
-		/* 	return (ft_perror("Corrupted\n", 0)); */
+		if (nm_elf(ptr + sizeof(*ar), offset, bin, opt))
+			return (ft_perror("Corrupted\n", 0));
 		ptr += sizeof(struct ar_hdr) + ft_atoi(ar->ar_size);
 		free(name);
 	}
