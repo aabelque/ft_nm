99d4855a33bd2919e821d5da8c08ecaf385a0b2c
aabelque <aabelque@student.42.fr>
2021-05-03 13:20:54 +0200

    test elf on linux

--- srcs/srcs_linux/ar_elf.c
+++ srcs/srcs_linux/ar_elf.c
@@ -6,7 +6,7 @@
 /*   By: azziz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 14:42:59 by azziz             #+#    #+#             */
-/*   Updated: 2021/05/03 13:20:40 by azziz            ###   ########.fr       */
+/*   Updated: 2021/05/03 13:20:53 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,7 +96,7 @@ int				ar_elf(char *ptr, char *offset, char *bin, int opt)
 		ar = (struct ar_hdr *)ptr;
 		/* printc('\n'); */
 		prints(name);
-		/* printc('\n'); */
+		printc('\n');
 		/* if (name) */
 		/* 	print_archive(name); */
 		/* else */
