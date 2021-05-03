b40745d0c05606a80b6ac5e19752b9c4adfd14e2
aabelque <aabelque@student.42.fr>
2021-05-03 12:56:35 +0200

    test elf on linux

--- srcs/srcs_linux/ar_elf.c
+++ srcs/srcs_linux/ar_elf.c
@@ -6,7 +6,7 @@
 /*   By: azziz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 14:42:59 by azziz             #+#    #+#             */
-/*   Updated: 2021/05/03 12:49:18 by azziz            ###   ########.fr       */
+/*   Updated: 2021/05/03 12:56:34 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,10 @@ static char		*get_name(char **strtab, int str_idx, int size) {
 	name = NULL;
 	while (!('a' <= **strtab && **strtab <= 'z'))
 		(*strtab)++;
-	while (*strtab[idx] != '/')
+	while (*strtab[idx])
 	{
+		if (*strtab[idx] == '/')
+			break ;
 		printc(*strtab[idx]);
 		idx++;
 	}
