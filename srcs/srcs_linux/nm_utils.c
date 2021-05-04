641ad61c9c09eb5e828f5eebe5f186191fcd1967
aabelque <aabelque@student.42.fr>
2021-05-04 18:29:25 +0200

    test elf on linux

--- srcs/srcs_linux/nm_utils.c
+++ srcs/srcs_linux/nm_utils.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 17:43:06 by aabelque          #+#    #+#             */
-/*   Updated: 2021/05/04 18:24:28 by azziz            ###   ########.fr       */
+/*   Updated: 2021/05/04 18:29:14 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,11 +47,20 @@ void		ft_qsort_symelf(t_elf_symbol *sym, int size, int (*comp)(const char *, con
 		r = comp(sym[idx].name, pivot.name);
 		if (r <= 0) {
 			if (r == 0) {
-				if (sym[idx].value > pivot.value) {
-					tmp = sym[idx];
-					sym[idx] = sym[wall];
-					sym[wall] = tmp;
+				prints(sym[idx].name);
+				prints(" - ");
+				prints(pivot.name);
+				prints(" ( ");
+				ft_putnbr(idx);
+				prints(" - ");
+				ft_putnbr(wall);
+				prints(" ) ");
+				if (sym[idx].value != pivot.value) {
+					hexdump(sym[idx].value, 16, 16);
+					prints(" - ");
+					hexdump(pivot.value, 16, 16);
 				}
+				printc('\n');
 			}
 			if (wall != idx) {
 				tmp = sym[idx];
