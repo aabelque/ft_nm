cce903fb2cea185ea5de50125e284cbd77c1f993
aabelque <aabelque@student.42.fr>
2021-05-05 15:57:37 +0200

    test elf on linux

--- srcs/srcs_linux/elf32.c
+++ srcs/srcs_linux/elf32.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 16:49:57 by aabelque          #+#    #+#             */
-/*   Updated: 2021/05/05 15:57:05 by azziz            ###   ########.fr       */
+/*   Updated: 2021/05/05 15:57:36 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,9 +89,9 @@ int							elf32(char *ptr, char *offset, int opt) {
 	t_elf_section	*sections = NULL;
 
 	eh = (Elf32_Ehdr *)ptr;
-	ft_putnbr(&offset);
+	ft_putnbr((int)&offset);
 	printc('\n');
-	ft_putnbr(&(ptr + eh->e_shoff));
+	ft_putnbr((ptr + eh->e_shoff));
 	printc('\n');
 	return (0);
 	/* if (check_offset_elf(ptr + eh->e_shoff, offset)) */
