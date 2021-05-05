af7b40dcc9457513f6d46a1eca3ee832fb4d3733
aabelque <aabelque@student.42.fr>
2021-05-05 16:15:20 +0200

    test elf on linux

--- srcs/srcs_linux/flags_utils.c
+++ srcs/srcs_linux/flags_utils.c
@@ -6,11 +6,21 @@
 /*   By: azziz <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 15:30:46 by azziz             #+#    #+#             */
-/*   Updated: 2021/05/05 16:08:09 by azziz            ###   ########.fr       */
+/*   Updated: 2021/05/05 16:15:17 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_nm.h"
+
+static char	progbits_alloc(t_elf_symbol sym) {
+	if (sym.bind == STB_WEAK) {
+		if (sym.type == STT_OBJECT)
+			return (sym.shndx == SHN_UNDEF ? 'v' : 'V');
+	}
+	if (sym.bind == STB_LOOS)
+		return ('u');
+	return (sym.bind == STB_LOCAL ? 'r' : 'R');
+}
 
 static char	progbits_alloc_write(t_elf_symbol sym) {
 	if (sym.bind == STB_WEAK) {
@@ -33,24 +43,16 @@ char		progbits_flag(t_elf_symbol sym, t_elf_section *sections) {
 			return (sym.shndx == SHN_UNDEF ? 'w' : 'W');
 		return (sym.bind == STB_LOCAL ? 't' : 'T');
 	}
-	else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE)) {
+	else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE))
 		return (progbits_alloc_write(sym));
-	}
 	else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE | SHF_EXECINSTR))
 		return (sym.bind == STB_LOCAL ? 't' : 'T');
 	else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_MERGE))
 		return (sym.bind == STB_LOCAL ? 'r' : 'R');
 	else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_MERGE | SHF_STRINGS))
 		return (sym.bind == STB_LOCAL ? 'r' : 'R');
-	else if (sections[sym.shndx].flag == SHF_ALLOC) {
-		if (sym.bind == STB_WEAK) {
-			if (sym.type == STT_OBJECT)
-				return (sym.shndx == SHN_UNDEF ? 'v' : 'V');
-		}
-		if (sym.bind == STB_LOOS)
-			return ('u');
-		return (sym.bind == STB_LOCAL ? 'r' : 'R');
-	}
+	else if (sections[sym.shndx].flag == SHF_ALLOC)
+		return (progbits_alloc(sym));
 	else if (sections[sym.shndx].flag == ((sections[sym.shndx].flag & SHF_MASKOS) | SHF_ALLOC))
 		return (sym.bind == STB_LOCAL ? 'r' : 'R');
 	else if (sections[sym.shndx].flag == ((sections[sym.shndx].flag & SHF_MASKPROC) | (SHF_ALLOC | SHF_WRITE)))
