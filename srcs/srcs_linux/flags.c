a7e47f722a38fc5ec005b7ab8acc3a03f39bc510
aabelque <aabelque@student.42.fr>
2021-05-05 15:40:54 +0200

    test elf on linux

--- srcs/srcs_linux/flags.c
+++ srcs/srcs_linux/flags.c
@@ -6,7 +6,7 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 15:24:05 by aabelque          #+#    #+#             */
-/*   Updated: 2021/05/05 15:36:14 by azziz            ###   ########.fr       */
+/*   Updated: 2021/05/05 15:40:53 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,16 +84,6 @@ char			get_flags(t_elf_symbol sym, t_elf_section *sections) {
 
 	if (sym.shndx > MAX_SECTIONS || sym.shndx == SHN_ABS)
 		return (sym.bind == STB_LOCAL ? 'a' : 'A');
-	/* prints(sections[sym.shndx].name); */
-	/* prints(" -->  "); */
-	/* ft_putnbr(sections[sym.shndx].flag); */
-	/* prints(" "); */
-	/* ft_putnbr(SHF_ALLOC | SHF_WRITE); */
-	/* prints(" "); */
-	/* /1* ft_putnbr((sections[sym.shndx].flag & SHF_MASKOS) | SHF_ALLOC); *1/ */
-	/* prints(" "); */
-	/* ft_putnbr((sections[sym.shndx].flag & SHF_MASKPROC) | (SHF_ALLOC | SHF_WRITE)); */
-	/* prints(" "); */
 	if (sections[sym.shndx].name == NULL) {
 		if (sym.bind == STB_WEAK) {
 			if (sym.type == STT_OBJECT)
@@ -106,44 +96,7 @@ char			get_flags(t_elf_symbol sym, t_elf_section *sections) {
 			return (sym.bind == STB_WEAK ? 'w' : 'U');
 	}
 	else if (sections[sym.shndx].type == SHT_PROGBITS) {
-		if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_EXECINSTR)) {
-			if (sym.bind == STB_WEAK)
-				return (sym.shndx == SHN_UNDEF ? 'w' : 'W');
-			return (sym.bind == STB_LOCAL ? 't' : 'T');
-		}
-		else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE)) {
-			if (sym.bind == STB_WEAK) {
-				if (sym.type == STT_OBJECT)
-					return (sym.shndx == SHN_UNDEF ? 'v' : 'V');
-				return (sym.shndx == SHN_UNDEF ? 'w' : 'W');
-			}
-			else if (sym.bind == STB_GLOBAL) {
-				if (sym.type == STT_OBJECT)
-					return (sym.shndx == SHN_UNDEF ? 'G' : 'D');
-			}
-			else if (sym.bind == STB_LOOS)
-				return ('u');
-			return (sym.bind == STB_LOCAL ? 'd' : 'D');
-		}
-		else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE | SHF_EXECINSTR))
-			return (sym.bind == STB_LOCAL ? 't' : 'T');
-		else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_MERGE))
-			return (sym.bind == STB_LOCAL ? 'r' : 'R');
-		else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_MERGE | SHF_STRINGS))
-			return (sym.bind == STB_LOCAL ? 'r' : 'R');
-		else if (sections[sym.shndx].flag == SHF_ALLOC) {
-			if (sym.bind == STB_WEAK) {
-				if (sym.type == STT_OBJECT)
-					return (sym.shndx == SHN_UNDEF ? 'v' : 'V');
-			}
-			if (sym.bind == STB_LOOS)
-				return ('u');
-			return (sym.bind == STB_LOCAL ? 'r' : 'R');
-		}
-		else if (sections[sym.shndx].flag == ((sections[sym.shndx].flag & SHF_MASKOS) | SHF_ALLOC))
-			return (sym.bind == STB_LOCAL ? 'r' : 'R');
-		else if (sections[sym.shndx].flag == ((sections[sym.shndx].flag & SHF_MASKPROC) | (SHF_ALLOC | SHF_WRITE)))
-			return (sym.bind == STB_LOCAL ? 'g' : 'G');
+		return (progbits_flag(sym, sections));
 	}
 	else if (sections[sym.shndx].type == SHT_DYNAMIC) {
 		if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE))
