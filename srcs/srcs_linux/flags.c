d1827ad09b7de97db2da729cf3bbaf2a69c52624
aabelque <aabelque@student.42.fr>
2021-05-05 16:23:12 +0200

    test elf on linux

--- srcs/srcs_linux/flags.c
+++ srcs/srcs_linux/flags.c
@@ -6,61 +6,67 @@
 /*   By: aabelque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/25 15:24:05 by aabelque          #+#    #+#             */
-/*   Updated: 2021/05/05 16:19:45 by azziz            ###   ########.fr       */
+/*   Updated: 2021/05/05 16:23:10 by azziz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_nm.h"
 
+static char		noname_flag(t_elf_symbol sym) {
+	if (sym.bind == STB_WEAK) {
+		if (sym.type == STT_OBJECT)
+			return ('v');
+		return (sym.shndx == SHN_UNDEF ? 'w' : 'W');
+	}
+	else if (sym.bind == STB_LOCAL && sym.type == STT_NOTYPE)
+		return ('0');
+	if (sym.shndx == SHN_UNDEF)
+		return (sym.bind == STB_WEAK ? 'w' : 'U');
+}
+
+static char		progbits_ppc(t_elf_symbol sym, t_elf_section *sections) {
+	if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_EXECINSTR)) {
+		if (sym.bind == STB_WEAK)
+			return (sym.shndx == SHN_UNDEF ? 'w' : 'W');
+		return (sym.bind == STB_LOCAL ? 't' : 'T');
+	}
+	else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE)) {
+		if (sym.bind == STB_WEAK) {
+			if (sym.type == STT_OBJECT)
+				return (sym.shndx == SHN_UNDEF ? 'v' : 'V');
+			return (sym.shndx == SHN_UNDEF ? 'w' : 'W');
+		}
+		else if (sym.bind == STB_GLOBAL) {
+			if (sym.type == STT_OBJECT) {
+				if (!ft_strcmp(sections[sym.shndx].name, ".sdata"))
+					return ('G');
+				return ('D');
+			}
+			else if (sym.type == STT_NOTYPE) {
+				if (!ft_strcmp(sections[sym.shndx].name, ".sdata"))
+					return ('G');
+				return ('D');
+			}
+		}
+		return (sym.bind == STB_LOCAL ? 'd' : 'D');
+	}
+	else if (sections[sym.shndx].flag == SHF_ALLOC) {
+		if (sym.bind == STB_WEAK) {
+			if (sym.type == STT_OBJECT)
+				return (sym.shndx == SHN_UNDEF ? 'v' : 'V');
+		}
+		return (sym.bind == STB_LOCAL ? 'r' : 'R');
+	}
+}
+
 char			get_flags_ppc(t_elf_symbol sym, t_elf_section *sections) {
 
 	if (sym.shndx > MAX_SECTIONS || sym.shndx == SHN_ABS)
 		return (sym.bind == STB_LOCAL ? 'a' : 'A');
-	if (sections[sym.shndx].name == NULL) {
-		if (sym.bind == STB_WEAK) {
-			if (sym.type == STT_OBJECT)
-				return ('v');
-			return (sym.shndx == SHN_UNDEF ? 'w' : 'W');
-		}
-		else if (sym.bind == STB_LOCAL && sym.type == STT_NOTYPE)
-			return ('0');
-		if (sym.shndx == SHN_UNDEF)
-			return (sym.bind == STB_WEAK ? 'w' : 'U');
-	}
-	else if (sections[sym.shndx].type == SHT_PROGBITS) {
-		if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_EXECINSTR)) {
-			if (sym.bind == STB_WEAK)
-				return (sym.shndx == SHN_UNDEF ? 'w' : 'W');
-			return (sym.bind == STB_LOCAL ? 't' : 'T');
-		}
-		else if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE)) {
-			if (sym.bind == STB_WEAK) {
-				if (sym.type == STT_OBJECT)
-					return (sym.shndx == SHN_UNDEF ? 'v' : 'V');
-				return (sym.shndx == SHN_UNDEF ? 'w' : 'W');
-			}
-			else if (sym.bind == STB_GLOBAL) {
-				if (sym.type == STT_OBJECT) {
-					if (!ft_strcmp(sections[sym.shndx].name, ".sdata"))
-						return ('G');
-					return ('D');
-				}
-				else if (sym.type == STT_NOTYPE) {
-					if (!ft_strcmp(sections[sym.shndx].name, ".sdata"))
-						return ('G');
-					return ('D');
-				}
-			}
-			return (sym.bind == STB_LOCAL ? 'd' : 'D');
-		}
-		else if (sections[sym.shndx].flag == SHF_ALLOC) {
-			if (sym.bind == STB_WEAK) {
-				if (sym.type == STT_OBJECT)
-					return (sym.shndx == SHN_UNDEF ? 'v' : 'V');
-			}
-			return (sym.bind == STB_LOCAL ? 'r' : 'R');
-		}
-	}
+	if (sections[sym.shndx].name == NULL)
+		return (noname_flag(sym));
+	else if (sections[sym.shndx].type == SHT_PROGBITS)
+		return (progbits_ppc(sym, sections));
 	else if (sections[sym.shndx].type == SHT_NOBITS) {
 		if (sections[sym.shndx].flag == (SHF_ALLOC | SHF_WRITE)) {
 			if (sym.type == STT_OBJECT) {
@@ -78,18 +84,6 @@ char			get_flags_ppc(t_elf_symbol sym, t_elf_section *sections) {
 		}
 	}
 	return ('?');
-}
-
-static char		noname_flag(t_elf_symbol sym) {
-	if (sym.bind == STB_WEAK) {
-		if (sym.type == STT_OBJECT)
-			return ('v');
-		return (sym.shndx == SHN_UNDEF ? 'w' : 'W');
-	}
-	else if (sym.bind == STB_LOCAL && sym.type == STT_NOTYPE)
-		return ('0');
-	if (sym.shndx == SHN_UNDEF)
-		return (sym.bind == STB_WEAK ? 'w' : 'U');
 }
 
 char			get_flags(t_elf_symbol sym, t_elf_section *sections) {
